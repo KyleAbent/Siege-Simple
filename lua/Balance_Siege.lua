9f07328f1ff54201d8ddc63f06b8540e42aae229
Kyle <kyleabent@gmail.com>
2016-11-19 09:26:04 -0500

    11.19

---
+++ lua/Balance_Siege.lua
@@ -0,0 +1,6 @@
+kFlameThrowerEnergyDamage = 0 -- 1 if speed adjust doesn't work
+kSentryDamage =  4 --default 5
+kBlinkEnergyCost = 29 -- default 32
+kStartBlinkEnergyCost = 13 --default 14
+kBoneShieldArmorPerSecond = 79 --default 12
+kMucousShieldPercent = 0.20 --default 0.15
